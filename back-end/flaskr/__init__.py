440cecd7b2755f34ba000108d984876ae83ab917
mateusz <kowalczuk.mateusz010@gmail.com>
2025-03-12 21:08:45 +0100

    connect with database

--- back-end/flaskr/__init__.py
+++ back-end/flaskr/__init__.py
@@ -1,7 +1,39 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
+app = Flask(__name__)
+
+# Konfiguracja połączenia z PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:securepassword@localhost:5432/hotel_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Wyłączenie powiadomień o modyfikacjach
+
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    try:
+        # Wykonaj zapytanie do bazy, aby sprawdzić połączenie
+        result = db.session.execute(text('SELECT 1'))  # Proste zapytanie
+        return "Połączenie z bazą danych działa!"
+    except Exception as e:
+        return f"Nie udało się połączyć z bazą danych: {e}"
+
+@app.route('/test_db')
+def test_db():
+    try:
+        # Próbujemy pobrać dane z tabeli
+        users = db.session.execute(text('SELECT * FROM users LIMIT 1')).fetchall()
+        if users:
+            return f"Połączenie działa! Pierwszy użytkownik: {users[0]}"
+        else:
+            return "Połączenie działa, ale nie ma danych w tabeli 'users'."
+    except Exception as e:
+        return f"Nie udało się połączyć z bazą danych: {e}"
+    
 
 def create_app(test_config=None):
     # create and configure the app
