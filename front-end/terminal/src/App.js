e171caabd69515782ece9f47c5bd53969704e1b8
Kamil <kamil.gromko.stud@pw.edu.pl>
2025-05-22 12:30:51 +0200

    added protected routing

--- front-end/terminal/src/App.js
+++ front-end/terminal/src/App.js
@@ -1,27 +1,44 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import SearchPage from './pages/SearchPage';
-import RoomDetailPage from './pages/RoomDetailPage.js';
+import RoomDetailPage from './pages/RoomDetailPage';
 import MyReservationsPage from './pages/MyReservationsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>SYSTEM REZERWACJI</h1>
-      <Routes>
-        <Route path="/" element={<Navigate to="/search" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/reserve/:roomId" element={<RoomDetailPage />} />
-        <Route path="/my-reservations" element={<MyReservationsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/login"    element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      <Route
+        path="/search"
+        element={
+          <ProtectedRoute>
+            <SearchPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/room/:id"
+        element={
+          <ProtectedRoute>
+            <RoomDetailPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/my-reservations"
+        element={
+          <ProtectedRoute>
+            <MyReservationsPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
-
-export default App;
--- front-end/terminal/src/index.js
+++ front-end/terminal/src/index.js
@@ -1,11 +1,17 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
