946d59f64ab9d66db5b766a431945c819973f946
Kamil <kamil.gromko.stud@pw.edu.pl>
2025-05-22 12:34:32 +0200

    fix protected routing

--- front-end/terminal/src/App.js
+++ front-end/terminal/src/App.js
@@ -1,34 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import SearchPage from './pages/SearchPage';
-import RoomDetailPage from './pages/RoomDetailPage';
-import MyReservationsPage from './pages/MyReservationsPage';
-import NotFoundPage from './pages/NotFoundPage';
+
+import LoginPage           from './pages/LoginPage';
+import RegisterPage        from './pages/RegisterPage';
+import SearchPage          from './pages/SearchPage';
+import RoomDetailPage      from './pages/RoomDetailPage';
+import MyReservationsPage  from './pages/MyReservationsPage';
+import NotFoundPage        from './pages/NotFoundPage';
 
 export default function App() {
   return (
     <Routes>
+      {/* publiczne */}
       <Route path="/login"    element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
 
-      <Route
-        path="/search"
-        element={
-          <ProtectedRoute>
-            <SearchPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/room/:id"
-        element={
-          <ProtectedRoute>
-            <RoomDetailPage />
-          </ProtectedRoute>
-        }
-      />
+      {/* dostępne dla wszystkich */}
+      <Route path="/search"   element={<SearchPage />} />
+      <Route path="/room/:id" element={<RoomDetailPage />} />
+
+      {/* tylko dla zalogowanych */}
       <Route
         path="/my-reservations"
         element={
@@ -38,6 +29,7 @@ export default function App() {
         }
       />
 
+      {/* 404 */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
--- front-end/terminal/src/index.js
+++ front-end/terminal/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
